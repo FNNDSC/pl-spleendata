3176f207fdee02031f8f0ecf7fabeca31fd02df4
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2024-04-22 12:24:48 -0400

    Update with new repo

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,10 @@
 FROM docker.io/python:3.12.1-slim-bookworm
 
 LABEL org.opencontainers.image.authors="FNNDSC <dev@babyMRI.org>" \
-      org.opencontainers.image.title="ChRIS Plugin Title" \
-      org.opencontainers.image.description="A ChRIS plugin that..."
+      org.opencontainers.image.title="Spleen data downloader" \
+      org.opencontainers.image.description="A ChRIS FS plugin to download a set of spleen data"
 
-ARG SRCDIR=/usr/local/src/app
+ARG SRCDIR=/usr/local/src/pl-spleendata
 WORKDIR ${SRCDIR}
 
 COPY requirements.txt .
@@ -19,4 +19,4 @@ RUN pip install ".[${extras_require}]" \
     && cd / && rm -rf ${SRCDIR}
 WORKDIR /
 
-CMD ["commandname"]
+CMD ["spleendata"]
--- setup.py
+++ setup.py
@@ -19,18 +19,18 @@ def get_version(rel_path: str) -> str:
 
 
 setup(
-    name='chris-plugin-template',
-    version=get_version('app.py'),
-    description='A ChRIS DS plugin template',
+    name='spleendata',
+    version=get_version('spleendata.py'),
+    description='A ChRIS FS plugin to download a set of spleen data',
     author='FNNDSC',
     author_email='dev@babyMRI.org',
-    url='https://github.com/FNNDSC/python-chrisapp-template',
-    py_modules=['app'],
+    url='https://github.com/rudolphpienaar/pl-spleendata',
+    py_modules=['spleendata'],
     install_requires=['chris_plugin'],
     license='MIT',
     entry_points={
         'console_scripts': [
-            'commandname = app:main'
+            'spleendata = spleendata:main'
         ]
     },
     classifiers=[
